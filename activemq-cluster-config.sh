abbf61a939bf6d455fc1cc2982c1a0b4deeb01f1
Noel O'Connor <noel.oconnor@gmail.com>
2014-05-19 15:35:30 +1000

    Added check for no cluster members

--- activemq-cluster-config.sh
+++ activemq-cluster-config.sh
@@ -1,13 +1,19 @@
 #!/bin/bash 
 CLUSTER_NODES=$(env|grep ":61616"|grep -v `hostname -i`|cut -d \= -f 2)
-echo $CLUSTER_NODES
-echo "<networkConnectors>" > /tmp/file1
-for OUTPUT in $CLUSTER_NODES
-  do
-    echo "<networkConnector uri=\"static:("$OUTPUT")\"/>" >> /tmp/file1 
- done
-echo "</networkConnectors>" >> /tmp/file1
+	echo $CLUSTER_NODES
+if [ -z $CLUSTER_NODES ]
+then
+	cp activemq.xml activemq-run.conf
+else
+	echo $CLUSTER_NODES
+	echo "<networkConnectors>" > /tmp/file1
+	for OUTPUT in $CLUSTER_NODES
+  	do
+    		echo "<networkConnector uri=\"static:("$OUTPUT")\"/>" >> /tmp/file1 
+ 	done
+	echo "</networkConnectors>" >> /tmp/file1
 
-sed '/destinationPolicy/r /tmp/file1' activemq.xml > activemq-run.conf
+	sed '/destinationPolicy/r /tmp/file1' activemq.xml > activemq-run.conf
+fi
 cat activemq-run.conf
-/home/activemq/apache-activemq-5.9.0/bin/activemq console -Dactivemq.brokername=$HOSTNAME xbean:file:./activemqi-run.xml
+/home/activemq/apache-activemq-5.9.0/bin/activemq console -Dactivemq.brokername=$HOSTNAME xbean:file:./activemq-run.xml
